a00018c74b79564a5c0bf8465b51ff3893f4b017
Starxel <lucaslenaert@gmail.com>
2021-10-15 11:05:19 +0200

    commit

---
+++ tpv1.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+
+void estParfait(int nb){ 
+
+  int somme_diviseurs = 0; 
+
+  for (int i = 1; i < nb; i++) 
+
+    if (nb % i == 0) 
+
+      somme_diviseurs =  somme_diviseurs + i; 
+
+  if (somme_diviseurs == nb)
+
+    printf("Le nombre %d est parfait", nb); 
+} 
+
+ 
+
+void main(int argc, char *argv[]){   
+  
+  int n = atoi(argv[1]); 
+  
+  estParfait(n);
+	
+  return 0;
+  } 
+
+ 
